aa3d75b4b5b59377d5542ce5c222c5f1ebc4d3fb
Alex Merry <dev@randomguy3.me.uk>
2013-11-15 17:23:07 +0000

    Put in a fallback "text logo" if the site has not set a logo image

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -15,20 +15,13 @@
 
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div class="header__name-and-slogan" id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 class="header__site-name" id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div>
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name" id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+      <?php else: ?>
+        <h1 id="logo-text"><a href="/"><span class="logo-top">Giving</span> <span class="logo-bottom">What We Can</span></a></h1>
       <?php endif; ?>
 
       <?php print render($page['header']); ?>

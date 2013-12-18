680a6c7fa67c5b468a1f8010feab5a77741b7a5c
epithatree <robert@sainthilda.co.uk>
2013-12-18 11:49:50 +0000

    Added animations to the calculator

--- templates/calculator/calculator-results.php
+++ templates/calculator/calculator-results.php
@@ -63,7 +63,7 @@ $results['richtimes'] = 24;
             <label for "income">Annual Income:</label>
         </div>
         <div class="container inline">
-            <input  id="calc-howrich-incomenumber" class="long" type="text" name="annualIncome" size="6" onchange="calculateResults()" /> 
+            <input  id="calc-howrich-incomenumber" class="long" type="text" value="1000" name="annualIncome" size="6" onchange="calculateResults()" /> 
             <button name="currency" disabled>USD</button>
         </div> 
         <div class="container inline">
@@ -76,39 +76,43 @@ $results['richtimes'] = 24;
             <input id="calc-howrich-householdsize-child" class="short" type="text" name="householdSizeChild" value="0" onchange="calculateResults()"/>
         </div>
         <div class="stretch"></div>
-      <div class="container inline">
-      </div>
-    </div>
-  </div>
-  <div id="results">
-    <div class="results center tcenter">
-      <p class="page-leader"> You are in the richest <span class="red bold"><span id="richpercent">0</span>%</span> of the world's population. <br>
-      Your income is more than <span class="red bold"> <span id="richtimes">0</span> times</span> the global average. </p>
-      </div>
-    <div class="donation-selector">
-      <div class="donation-text-box">
-       <p class="bold"> If you were to donate <span id="percentage">10</span>% of your income: </p>  
-       <div id="slider"></div>
-       <p> You would still be in the richest <span id="netRichPercent">100</span>  of the world's population. Your income would still be more than <span id="netRichTimes">0</span>times the global average. </p>
-       <p class="bold" > And with your donation, every year... </p>
-       </div>
-       <b class="notch"></b>
-       <div class="outer-dial-container">
-         <div class="right-decoration"> </div>
-         <div class="dial-circularmargin"> </div>
-         <div class="inner-dial-container">
-           <input data-readonly="true" type="text" value="90" class="knob" data-width="300" data-height="300" data-max="100" data-min="0" data-fgcolor="#ffa800" data-bgcolor="rgb(108,0,0)" data-linecap="round">
+        <div class="container inline">
           </div>
-        <b class="notch">
         </div>
       </div>
-    <div class="container center tcenter">
-      <div class="container inline">
-        <?php include path_to_theme()."/templates/sharing/socialshare.php" ?>
+    <div id="results">
+      <div class="results center tcenter">
+        <p class="page-leader"> You are in the richest <span class="red bold"><span id="richpercent">0</span>%</span> of the world's population. <br>
+        Your income is more than <span class="red bold"> <span id="richtimes">0</span> times</span> the global average. </p>
+        </div>
+      <div class="donation-selector">
+        <div class="donation-selector-row">
+          <div class="donation-text-box">
+            <p class="bold"> If you were to donate <span id="percentage">10</span>% of your income: </p>  
+            <div id="slider"></div>
+            <p> You would still be in the richest <span id="netRichPercent">100</span>  of the world's population. Your income would still be more than <span id="netRichTimes">0</span> times the global average. </p>
+            <p class="bold" > And with your donation, every year... </p>
+          </div>
+          <b class="notch"></b>
+          <div class="outer-dial-container">
+              <div class="right-decoration"> </div>
+              <div class="dial-circularmargin"> </div>
+              <div class="front-dial-container">
+                <input data-readonly="true" type="text" value="0" class="knob front" data-width="300" data-height="300" data-max="100" data-min="0" data-fgcolor="#ffa800" data-bgcolor="rgba(0,0,0,0)" data-linecap="round" angleArc="360">
+              </div>
+              <div class="back-dial-container">
+                <input data-readonly="true" type="text" value="0" class="knob back" data-width="300" data-height="300" data-max="100" data-min="0" data-fgcolor="rgb(108,0,0)" data-bgcolor="rgba(0,0,0,0)" data-linecap="round" angleArc="360">  
+              <b class="notch">
+            </div>
+          </div>
+      </div>
+      <div class="container center tcenter">
+        <div class="container inline">
+          <?php include path_to_theme()."/templates/sharing/socialshare.php" ?>
         </div>
       </div>
-    <div class="container" id="AMF"> </div>
-    <div class="container" id="SCI"> </div>   
+      <div class="container" id="AMF"> </div>
+      <div class="container" id="SCI"> </div>   
     </div>
   </div>
 </div>
@@ -121,8 +125,6 @@ $results['richtimes'] = 24;
             max: 100,
             slide: function(event, ui) {
                 jQuery('#percentage').text(ui.value);
-                jQuery('.knob').val(100-ui.value);
-                jQuery('.knob').trigger('change');
                 window.wealthCalculator.donationAmount = ui.value; 
                 window.wealthCalculator.potentialWealth();
             }
@@ -132,13 +134,13 @@ $results['richtimes'] = 24;
     jQuery(function() {
         jQuery(".knob").knob({
                 displayInput: false,
-                angleArc: 334,
-                thickness: ".15"
+                angleArc:"360",
+                thickness: ".15",
             });
     });
 
     jQuery(function() {
-      //jQuery("#results").hide();
+      fadeInBack();
     });
 
     jQuery(function(){
@@ -147,6 +149,9 @@ $results['richtimes'] = 24;
     });
     jQuery(function(){
       window.wealthCalculator = new wealthCalculator("US", 0, 1, 0);
+      window.wealthCalculator.updateWealth();
+      window.wealthCalculator.calculateWealth();
+      window.wealthCalculator.potentialWealth();
     });
     function wealthCalculator(country, income, adult, child) {
       this.country = country;
@@ -176,23 +181,24 @@ $results['richtimes'] = 24;
         this.income = this.income*((100-this.donationAmount)/100);
         this.richFactorNum = richFactor(this.income);
         this.richPercentageNum = richPercentage(this.income);
-        updateDisplay(this.richFactorNum, this.richPercentageNum);
      }
      this.potentialWealth = potentialWealth;
      function potentialWealth(){
         this.potentialIncome = this.income*((100-this.donationAmount)/100);
-        updatePotentialDisplay(richFactor(this.potentialIncome), richPercentage(this.potentialIncome));
      }
    }
 
   function showResults(){
-    jQuery("#results").show();
+    jQuery(".donation-text-box").css('visibility','visible').hide().fadeIn('slow');
+    jQuery(".right-decoration").css('visibility','visible').hide().fadeIn('slow');
+    fadeInFront();
    } 
   function calculateResults(){
     window.wealthCalculator.updateWealth();
     window.wealthCalculator.calculateWealth();
     window.wealthCalculator.potentialWealth();
-    showResults();
+    window.wealthCalculator.updateDisplay(window.wealthCalculator.richFactorNum, window.wealthCalculator.richPercentageNum);
+    updatePotentialDisplay(richFactor(wndow.wealthCalculator.potentialIncome), richPercentage(window.wealthCalculator.potentialIncome));
   }
   function updateCurrency(){
     jQuery(":button").text(getCurrency(jQuery("#calc-howrich-country option:selected").val()));
@@ -200,9 +206,29 @@ $results['richtimes'] = 24;
   function updateDisplay(richFactorNum, richPercentageNum){
       jQuery("#richpercent").text(richPercentageNum.toFixed(1));
       jQuery("#richtimes").text(richFactorNum.toFixed(1));
+      jQuery('.knob.back').val(100-richPercentageNum);
+      jQuery('.knob.back').trigger('change');
   }
   function updatePotentialDisplay(richFactor, richPercentage){
     jQuery("#netRichPercent").text(richPercentage.toFixed(1));
     jQuery("#netRichTimes").text(richFactor.toFixed(1));
+    jQuery('.knob.front').val(100-richPercentage);
+    jQuery('.knob.front').trigger('change');
+  }
+  function fadeInBack(){
+      var i = 0;
+      var timer = setInterval(function(){
+      jQuery('.knob.back').val(i);
+      jQuery('.knob.back').trigger('change');
+      if (i < 100-window.wealthCalculator.richPercentageNum) { i = i+1;} else {clearInterval(timer); showResults();}
+      },100 );
+  }
+  function fadeInFront(){
+      var i = 0;
+      var timer = setInterval(function(){
+      jQuery('.knob.front').val(i);
+      jQuery('.knob.front').trigger('change');
+      if (i < 100-richPercentage(window.wealthCalculator.potentialIncome)) { i = i+1;} else {clearInterval(timer);}
+      },100 );
   }
 </script>
